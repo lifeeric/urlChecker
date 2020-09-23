2afeef75f9294a39d9be02636b0ada89ea29efa9
unknown <isabellaliu77@gmail.com>
2020-09-23 00:50:52 -0400

    first version of checking urls

---
+++ go.mod
@@ -0,0 +1,5 @@
+module tmp
+
+go 1.15
+
+require mvdan.cc/xurls/v2 v2.2.0
--- urlChecker.go
+++ urlChecker.go
@@ -4,7 +4,27 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
+	"net/http"
+
+	"mvdan.cc/xurls/v2"
 )
+
+func extractURL(str string) []string {
+	rxStrict := xurls.Strict()
+	foundUrls := rxStrict.FindAllString(str, -1)
+	return foundUrls
+}
+
+func checkURL(urls []string) {
+	for _, v := range urls {
+		resp, err := http.Get(v)
+		if err != nil {
+			print(err.Error())
+		} else {
+			fmt.Println(string(resp.StatusCode) + resp.Status)
+		}
+	}
+}
 
 func main() {
 	content, err := ioutil.ReadFile("urls.txt")
@@ -14,5 +34,9 @@ func main() {
 
 	textContent := string(content)
 	fmt.Println(textContent)
+
+	fmt.Println("--------------------------------------------")
+
+	checkURL(extractURL(textContent))
 
 }
